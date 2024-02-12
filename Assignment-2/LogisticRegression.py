a7980c0fb5a5576e5bea8b4892700b0c0ed528f2
BenHepditch <benhepditch@gmail.com>
2024-02-12 14:43:05 -0500

    add outline

---
+++ Assignment-2/LogisticRegression.py
@@ -0,0 +1,11 @@
+class LogisticRegression:
+    def __init__(self) -> None:
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
+    
+    
---
+++ Assignment-2/MultiClassRegression.py
@@ -0,0 +1,10 @@
+class MultiClassRegression:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
