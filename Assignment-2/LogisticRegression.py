a7b37a4d8ec890ef3242210913779c3b95f81f66
BenHepditch <benhepditch@gmail.com>
2024-02-13 11:18:20 -0500

    add more wire frame and copy over examples

--- Assignment-2/LogisticRegression.py
+++ Assignment-2/LogisticRegression.py
@@ -1,11 +1,47 @@
+import numpy as np
+
 class LogisticRegression:
-    def __init__(self) -> None:
-        pass
     
-    def fit(self, X, y):
-        pass
+    def __init__(self, add_bias=True, learning_rate=.1, epsilon=1e-4, max_iters=1e5, verbose=False):
+        self.add_bias = add_bias
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon                        #to get the tolerance for the norm of gradients 
+        self.max_iters = max_iters                    #maximum number of iteration of gradient descent
+        self.verbose = verbose
+        self.logistic = lambda z: 1./ (1 + np.exp(-z))
+
+    def gradient(self, x, y):
+        N,D = x.shape
+        yh = self.logistic(np.dot(x, self.w))    # predictions  size N
+        grad = np.dot(x.T, yh - y)/N        # divide by N because cost is mean over N points
+        return grad
+        
+    def fit(self, x, y):
+        if x.ndim == 1:
+            x = x[:, None]
+        if self.add_bias:
+            N = x.shape[0]
+            x = np.column_stack([x,np.ones(N)])
+        N,D = x.shape
+        self.w = np.zeros(D)
+        g = np.inf 
+        t = 0
+        # the code snippet below is for gradient descent
+        while np.linalg.norm(g) > self.epsilon and t < self.max_iters:
+            g = self.gradient(x, y)
+            self.w = self.w - self.learning_rate * g 
+            t += 1
+        
+        if self.verbose:
+            print(f'terminated after {t} iterations, with norm of the gradient equal to {np.linalg.norm(g)}')
+            print(f'the weight found: {self.w}')
+        return self
     
-    def predict(self, X):
-        pass
-    
-    
+    def predict(self, x):
+        if x.ndim == 1:
+            x = x[:, None]
+        Nt = x.shape[0]
+        if self.add_bias:
+            x = np.column_stack([x,np.ones(Nt)])
+        yh = self.logistic(np.dot(x,self.w))            #predict output
+        return yh
