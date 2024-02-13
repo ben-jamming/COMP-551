ab05feeed5a8c23eb4b674c53092806073839bed
BenHepditch <benhepditch@gmail.com>
2024-02-13 13:01:36 -0500

    add multiclass regression from lecture slides

--- Assignment-2/MultiClassRegression.py
+++ Assignment-2/MultiClassRegression.py
@@ -1,10 +1,21 @@
+import numpy as np
+
 class MultiClassRegression:
     
-    def __init__(self) -> None:
-        pass
-    
-    def fit(self, X, y):
-        pass
-    
+    def __init__(self, nFeatures, nClasses):
+        self.W = np.random.rand(nFeatures, nClasses)
+        
     def predict(self, X):
-        pass
+        y_pred = np.exp(np.matmul(X, self.W))
+        return y_pred / np.sum(y_pred, axis=1).reshape(X.shape[0], 1)
+    
+    def gradient(self, X, y):
+        return np.matmul(X.T, self.predict(X) - y) / X.shape[0]
+    
+    def cross_entropy(self, y, X):
+        return -np.sum(y * np.log(self.predict(X)))
+        
+    def fit(self, X, y, lr=0.005, niters=100):
+        for i in range(niters):
+            self.W -= lr * self.gradient(X, y)
+        
