8f547690321cdca9b33d09a605e68712c908390b
BenHepditch <benhepditch@gmail.com>
2024-02-20 08:59:28 -0500

    Fix indexing error in output

--- Assignment-2/LinearRegression.py
+++ Assignment-2/LinearRegression.py
@@ -3,21 +3,15 @@ import numpy as np
 class LinearRegression:
     def __init__(self, add_bias=True):
         self.add_bias = add_bias
-        self.N = None
         self.w = None
-        
+
     def fit(self, x, y):
-        if x.ndim == 1:
-            x = x[:, None]                         #add a dimension for the features
-        self.N = x.shape[0]
         if self.add_bias:
-            x = np.column_stack([x,np.ones(self.N)])    #add bias by adding a constant feature of value 1
-        #alternatively: self.w = np.linalg.inv(x.T @ x)@x.T@y
+            x = np.column_stack([x, np.ones(x.shape[0])])  # Add bias term
         self.w = np.linalg.lstsq(x, y, rcond=None)[0]
         return self
-    
+
     def predict(self, x):
         if self.add_bias:
-            x = np.column_stack([x,np.ones(self.N)])
-        yh = x@self.w                            
-        return yh
+            x = np.column_stack([x, np.ones(x.shape[0])])
+        return x @ self.w
